f6063d6604d7d723e01dab87537121934113fbb4
guy <ethannathanduvall2013@gmail.com>
2024-06-27 22:31:50 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/starsprinter92/awesome-dots
+cd awesome-dots
+cp ./.config/* ~/.config/
+sudo chmod +x './put in \usr\bin/*'
+sudo cp './put in \usr\bin/*' /bin
+echo Done! (INSTALLER UNTESTED, THINGS WILL BREAK)
