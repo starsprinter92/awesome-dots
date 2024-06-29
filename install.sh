7bd0c69787f82b5fd3e7ff68a70d5b88ef8e3ac8
guy <ethannathanduvall2013@gmail.com>
2024-06-28 22:05:16 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/starsprinter92/awesome-dots
 cd awesome-dots
 cp -r ./.config/* ~/.config/
-sudo chmod +x './put in \usr\bin/*'
-sudo cp -r './put in \usr\bin/*' /bin
+sudo chmod +x './put in \usr\bin/lock'
+sudo cp -r './put in \usr\bin/lock' /bin
 echo Done! '(INSTALLER UNTESTED, THINGS WILL BREAK)'
